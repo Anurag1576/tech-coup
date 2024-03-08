9821f3469448b5df809ebc2c6e17cc4430e64a33
anurag <jawaharmalanianurag@gmail.com>
2024-03-09 01:22:16 +0530

    add

---
+++ stufacdemo.py
@@ -0,0 +1,24 @@
+from student import student1 
+from faculty import Faculty
+
+# ob1 = Student("Vikas",21,"3242",321.12)
+# ob1.show()
+# ob2 = Faculty("Rajesh",29,"12wse",23321)
+# ob2.show()
+
+ob1 = student1()
+ob2 = Faculty()
+ob3 = student1()
+ob4 = Faculty()
+ob5 = student1()
+ob6 = Faculty()
+ob7 = student1()
+ob8 = Faculty()
+
+data = [ob1,ob2,ob3,ob4,ob5,ob6,ob7,ob8]
+print(Faculty.__name__)
+# for i in data:
+#     print(i.__name__)           
+    
+#print(student1.__dict__)
+#print(student1.__name__)       
