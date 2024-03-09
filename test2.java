d801d6c828e76dc1d6db82c4abc28b07579aa391
gokulchoudhary <gokulchoudhary1432@gmail.com>
2024-03-09 13:29:53 +0530

    adding new code

---
+++ test2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class test2 {
+
+	public static void main(String[] args)
+	{
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter num");
+		int num;
+		num=sc.nextInt();
+		boolean z= num<=100;
+		 System.out.println(z);
+		
+		
+	}
+	
+}
